ab2cfd2221c038bc312174e3d217444b596926c6
Bogdan301 <82769336+Bogdan301@users.noreply.github.com>
2021-07-20 15:31:12 +0300

    Solar panel

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $(".info-tabs_item").on('click', function (e){
+        e.preventDefault();
+
+        $('.info-tabs_item').removeClass('info-tabs_item--active');
+
+    });
+
+});
+
+console.log(1)
